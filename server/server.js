d72a669fff1684186cc58d02c7d6c95ada9bb9af
Guczu <jakub180720@gmail.com>
2023-03-26 16:04:49 +0200

    add mongodb connection, product model, get all products endpoint

--- server/server.js
+++ server/server.js
@@ -2,8 +2,34 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const Product = require('./model/Product');
 
 const app = express();
 app.use(bodyParser.json());
 const port = process.env.PORT || 5000;
 const dbURL = process.env.MONGODB_URL;
+
+mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Error connecting to MongoDB:', err));
+
+const router = express.Router();
+app.use(router);
+
+router.get('/', (req, res) => {
+    res.send('main route');
+});
+
+//get all products from db
+app.get("/products", async (req, res) => {
+    try {
+      const products = await Product.find();
+      res.json(products);
+    } catch (err) {
+      console.error("Error fetching products:", err);
+      res.status(500).send("Error fetching products");
+    }
+  });
+
+
+app.listen(port, () => console.log(`Listening on port ${port}...`));
