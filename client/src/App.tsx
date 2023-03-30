18d5a16209a97ada8208d4184cabc06a43db36af
Guczu <jakub180720@gmail.com>
2023-03-30 18:41:52 +0200

    added react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home'
 
 const App: React.FC = () => {
   return (
     <div className='App'>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   )
 }
